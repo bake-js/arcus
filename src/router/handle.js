d556d0c1f1f104c9342b017e6397595c1359869e
deMGoncalves <cleber.engineer@hotmail.com>
2024-08-23 13:14:30 -0300

    docs: melhora a legibilidade do codigo handle

--- src/router/handle.js
+++ src/router/handle.js
@@ -3,13 +3,19 @@ import extractBody from "./extractBody";
 import extractParams from "./extractParams";
 import findMatchingRoute from "./findMatchingRoute";
 
+// Função principal que lida com a requisição
 async function handle(request, env, ctx) {
+  // Encontra a rota correspondente com base na requisição
   const { page, path } = findMatchingRoute(request);
 
+  // Extrai o corpo da requisição
   await extractBody(request);
+
+  // Extrai argumentos e parâmetros da requisição
   extractArgs(request);
   extractParams(request, path);
 
+  // Retorna a página correspondente, passando a requisição, o ambiente e o contexto
   return page(request, env, ctx);
 }
 
