bb0ea574c0ce3e9f0aeeafafbe66af29bd67be14
deMGoncalves <cleber.engineer@hotmail.com>
2024-08-23 13:13:06 -0300

    docs: documenta a implementacao do router

--- src/router/router.js
+++ src/router/router.js
@@ -1,13 +1,22 @@
 import handle from "./handle";
 import listeners from "./listeners";
 
+// Cria um objeto proxy para o roteador
 const router = new Proxy(
   {},
   {
+    // Intercepta o acesso às propriedades do objeto
     get(_, method) {
-      if (/handle/.test(method)) return handle;
-      return (path, page, name) =>
-        listeners[method.toUpperCase()].push({ path, page, name });
+      // Se o método solicitado contém "handle", retorna a função `handle`
+      if (/handle/.test(method)) {
+        return handle;
+      }
+
+      // Para outros métodos, adiciona uma nova rota aos listeners
+      return (path, page, name) => {
+        const methodName = method.toUpperCase();
+        listeners[methodName].push({ path, page, name });
+      };
     },
   },
 );
